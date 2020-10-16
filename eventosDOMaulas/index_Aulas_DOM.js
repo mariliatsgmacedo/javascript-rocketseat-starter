e80b8666ec2b66324d5d477bc936205400f697be
Marilia Macedo <maritamisguemacedo@gmail.com>
2020-10-16 22:47:19 +0100

    Exercicio 2 - OK

--- eventosDOMaulas/index_Aulas_DOM.js
+++ eventosDOMaulas/index_Aulas_DOM.js
@@ -8,9 +8,8 @@ function mostraAlerta() {
 var inputElement = document.querySelector('input[name=nome]')
 var btnElement = document.querySelector('button.botao')
 
-btnElement.onclick = function(){
+btnElement.onclick = function () {
     var text = inputElement.value
-    //alert('Botão clicado')
     alert(text)
 }
 
@@ -18,25 +17,25 @@ btnElement.onclick = function(){
  * Aula 3
  */
 
- var linkElement = document.createElement('a')
- linkElement.setAttribute('href','https://google.com')
+var linkElement = document.createElement('a')
+linkElement.setAttribute('href', 'https://google.com')
 
- var textElement = document.createTextNode('Acessar Google')
- linkElement.appendChild(textElement)
+var textElement = document.createTextNode('Acessar Google')
+linkElement.appendChild(textElement)
 
- var containerElement = document.querySelector('#app')
- containerElement.appendChild(linkElement)
+var containerElement = document.querySelector('#app')
+containerElement.appendChild(linkElement)
 
- var inputElemento = document.querySelector('#nnome')
- containerElement.removeChild(inputElemento)
+var inputElemento = document.querySelector('#nnome')
+containerElement.removeChild(inputElemento)
 
 
- /**
-  * Aula 4
-  */
+/**
+ * Aula 4
+ */
 
-  var boxElement = document.querySelector('.caixa')
+var boxElement = document.querySelector('.caixa')
 
-  boxElement.style.width = '100px'
-  boxElement.style.height = '100px'
-  boxElement.style.backgroundColor = 'green';
+boxElement.style.width = '100px'
+boxElement.style.height = '100px'
+boxElement.style.backgroundColor = 'green';
--- eventosDOMexercicios/index-exercicios.js
+++ eventosDOMexercicios/index-exercicios.js
@@ -4,16 +4,34 @@ function addBox() {
     //Create Element
     var boxDiv = document.createElement('div')
     boxDiv.setAttribute('id', 'boxRed')
+    boxDiv.setAttribute('onmouseenter', 'changeColorBack(this)')
     //Create Properties
     boxDiv.style.width = '100px'
     boxDiv.style.height = '100px'
     boxDiv.style.backgroundColor = '#f00'
     boxDiv.style.margin = '10px'
     boxDiv.style.display = 'inline-block'
-    
     //Add Body
     document.body.appendChild(boxDiv)
+
 }
+
+function changeColorBack(boxDiv) {
+    boxDiv.style.backgroundColor = getRandomColor()
+}
+
+function getRandomColor() {
+
+    var letters = '0123456789ABCDEF'
+    var color = '#'
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+
+    return color
+}
+var newColor = getRandomColor()
 
 botao.addEventListener('click', addBox)
 
